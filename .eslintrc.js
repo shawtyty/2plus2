0043f96e070c2a0f5145943bb13b573b6a542c59
shawty <60017026+shawtyty@users.noreply.github.com>
2020-06-16 10:59:36 +0300

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  plugins: ['import'],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'no-restricted-globals' : 'off',
+    'no-sequences' : 'off',
+    'no-restricted-globals' : 'off',
+    'func-names' : 'off',
+    'no-unused-expressions' : 'off',
+  },
+};
